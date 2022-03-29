c1f33c91277b54faa6cbbf502143f2419580c4f7
Laura Bigoni <laurabigoni.92@gmail.com>
2022-03-29 15:43:25 +0200

    creating foreign key

---
+++ database/migrations/2022_03_29_133742_add_category_id_to_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCategoryIdToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // * Forma lunga
+            // Aggiungo la colonna
+            $table->unsignedBigInteger('category_id')->nullable()->after('id');
+            // Aggiungo la relazione 
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
+
+            // * Forma breve
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
+    }
+}
