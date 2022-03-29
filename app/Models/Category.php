8b1fd4731f6590783b220aa0086dd0aed47580a8
Laura Bigoni <laurabigoni.92@gmail.com>
2022-03-29 18:14:42 +0200

    add protected function fillable in category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,11 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 
 class Category extends Model
-{   
+{
     /** 
      * Serve per fare $category->posts
      */
-    public function posts() {
+    public function posts()
+    {
         return $this->hasMany('App\Models\Post');
     }
+
+    protected $fillable = ['label', 'color'];
 }
